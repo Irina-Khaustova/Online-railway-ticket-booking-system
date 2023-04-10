770e0b3457250f3c46e147265440b517a1236243
Irina <irina170580@yandex.ru>
2023-04-10 22:23:18 +0300

    second commit

--- src/components/mainPaige/MainPageHeader.js
+++ src/components/mainPaige/MainPageHeader.js
@@ -1,5 +1,5 @@
 import Menu from "./Menu";
-//import MyImage from "../img/banner.jpg";
+import MyImage from "../../img/552ec05b23bca029a735fc146cf0ebff.png";
 import SearchTrainForm from "./SearchTrainForm";
 
 export default function MainPageHeader() {
@@ -8,15 +8,17 @@ export default function MainPageHeader() {
       <div className="container main-page-container">
         <div className="logo"></div>
         <Menu className="menu"></Menu>
-
+        <img className="img-nain-page-header" src={MyImage} alt="К весне готовы!"></img>
         <div className="main-page-header-title">
           <div className="banner-header">
-            <span> Вся жизнь - </span>
-            <span className="bold"> путешествия!</span>
+            <span className="thin"> Вся жизнь - </span>
+            <span className="thick"> путешествия!</span>
           </div>
           </div>
           <SearchTrainForm></SearchTrainForm>
+          <div className="try"></div>
       </div>
     </>
   );
 }
+
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -2,6 +2,7 @@ import MainPageHeader from "../components/mainPaige/MainPageHeader";
 import AboutUs from "../components/mainPaige/AboutUs";
 import HowItsWorks from "../components/mainPaige/HowItsWorks";
 import Footer from "../components/Footer";
+import Reviews from "../components/mainPaige/Reviews";
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 
@@ -13,10 +14,11 @@ export default function MainPage() {
   // }, []);
   return (
     <>
-      <MainPageHeader className="header" />
-      <AboutUs className="sales-hits" />
-      <HowItsWorks className="catalog" />
-      <Footer />
+      <MainPageHeader/>
+      <AboutUs/>
+      <HowItsWorks/>
+      <Reviews/>
+      <Footer/>
     </>
   );
 }
