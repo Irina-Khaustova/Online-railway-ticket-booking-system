207833d50016a601c5a07e1c31a2bf28c9333a2a
Irina <irina170580@yandex.ru>
2023-12-13 20:51:41 +0300

    v11

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -3,8 +3,6 @@ import AboutUs from "../components/mainPaige/AboutUs";
 import HowItsWorks from "../components/mainPaige/HowItsWorks";
 import Footer from "../components/Footer";
 import Reviews from "../components/mainPaige/Reviews";
-//import { useDispatch } from "react-redux";
-//import { useEffect } from "react";
 
 export default function MainPage() {
   
